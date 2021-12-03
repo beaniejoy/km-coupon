b480eab07f607595845a351e5717f43abb43f316
beanie.joy <beanie.joy@kakaomobility.com>
2021-12-03 15:16:18 +0900

    initial setting

---
+++ src/main/resources/db/migration/V1__Insert_coupon_test_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO coupon(id, use_min_amount, discount_amount, usable_from, usable_until, status, created_at, created_by)
+VALUES (1001, 5000, 3000, '2021-01-01 00:00:00', '2022-11-30 23:59:59', 'NORMAL', '2021-01-01 00:00:00', 'admin');
+
+INSERT INTO coupon(id, use_min_amount, discount_amount, usable_from, usable_until, status, created_at, created_by)
+VALUES (1002, 5000, 500, '2021-01-01 00:00:00', '2022-11-30 23:59:59', 'NORMAL', '2021-01-01 00:00:00', 'admin');
+
+INSERT INTO coupon(id, use_min_amount, discount_amount, usable_from, usable_until, status, created_at, created_by)
+VALUES (1003, 5000, 3000, '2021-01-01 00:00:00', '2022-11-30 23:59:59', 'NORMAL', '2021-01-01 00:00:00', 'admin');
+
+INSERT INTO coupon(id, use_min_amount, discount_amount, usable_from, usable_until, status, created_at, created_by)
+VALUES (1004, 5000, 3000, '2021-01-01 00:00:00', '2022-11-30 23:59:59', 'USED', '2021-01-01 00:00:00', 'admin');
+
+INSERT INTO coupon(id, use_min_amount, discount_amount, usable_from, usable_until, status, created_at, created_by)
+VALUES (1005, 5000, 3000, '2021-01-01 00:00:00', '2022-11-30 23:59:59', 'NORMAL', '2021-01-01 00:00:00', 'admin');
+
+INSERT INTO coupon(id, use_min_amount, discount_amount, usable_from, usable_until, status, created_at, created_by)
+VALUES (1006, 5000, 3000, '2021-07-01 00:00:00', '2022-11-30 23:59:59', 'NORMAL', '2021-07-01 00:00:00', 'admin');
