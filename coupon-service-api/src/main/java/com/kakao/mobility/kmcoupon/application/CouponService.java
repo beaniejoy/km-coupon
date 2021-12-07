e6b958b539be0cf9ca3917143ce47c176864554b
beaniejoy <lhb1025@gmail.com>
2021-12-08 04:11:29 +0900

    coupon domain processor 및 validator 제거

--- coupon-service-api/src/main/java/com/kakao/mobility/kmcoupon/application/CouponService.java
+++ coupon-service-api/src/main/java/com/kakao/mobility/kmcoupon/application/CouponService.java
@@ -1,16 +1,18 @@
 package com.kakao.mobility.kmcoupon.application;
 
-import com.kakao.mobility.kmcoupon.application.processor.CouponProcessor;
 import com.kakao.mobility.kmcoupon.domain.coupon.Coupon;
 import com.kakao.mobility.kmcoupon.domain.coupon.Status;
 import com.kakao.mobility.kmcoupon.dto.CouponUsingRequest;
 import com.kakao.mobility.kmcoupon.exception.CouponErrorMessage;
 import com.kakao.mobility.kmcoupon.exception.EntityNotFoundException;
+import com.kakao.mobility.kmcoupon.exception.InvalidRequestException;
+import com.kakao.mobility.kmcoupon.exception.InvalidStatusException;
 import com.kakao.mobility.kmcoupon.repository.CouponRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,11 +21,9 @@ import java.util.List;
 public class CouponService {
 
     private final CouponRepository couponRepository;
-    private final CouponProcessor couponProcessor;
 
-    public CouponService(CouponRepository couponRepository, CouponProcessor couponProcessor) {
+    public CouponService(CouponRepository couponRepository) {
         this.couponRepository = couponRepository;
-        this.couponProcessor = couponProcessor;
     }
 
     @Transactional(readOnly = true)
@@ -36,7 +36,19 @@ public class CouponService {
         Long couponId = couponUsingRequest.getCouponId();
         Coupon coupon = couponRepository.findByIdAndMemberId(couponId, memberId)
                 .orElseThrow(() -> new EntityNotFoundException(CouponErrorMessage.COUPON_NOT_FOUND));
-        couponProcessor.useCoupon(coupon, couponUsingRequest);
+
+        if (coupon.isAlreadyUsed())
+            throw new InvalidStatusException(CouponErrorMessage.COUPON_ALREADY_USED);
+
+        BigDecimal itemAmount = couponUsingRequest.getItemAmount();
+        if (coupon.isMinAmountBiggerThan(itemAmount))
+            throw new InvalidRequestException(CouponErrorMessage.COUPON_MIN_AMOUNT_NOT_SATISFIED);
+
+        LocalDateTime requestReceivedTime = couponUsingRequest.getRequestReceivedAt();
+        if (coupon.isNotUsableDuration(requestReceivedTime))
+            throw new InvalidRequestException(CouponErrorMessage.COUPON_NOT_USABLE_DURATION);
+
+        coupon.used();
 
         return coupon;
     }
--- coupon-service-api/src/main/java/com/kakao/mobility/kmcoupon/application/processor/CouponProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.kakao.mobility.kmcoupon.application.processor;
-
-import com.kakao.mobility.kmcoupon.domain.coupon.Coupon;
-import com.kakao.mobility.kmcoupon.dto.CouponUsingRequest;
-
-public interface CouponProcessor {
-    void useCoupon(Coupon coupon, CouponUsingRequest couponUsingRequest);
-}
